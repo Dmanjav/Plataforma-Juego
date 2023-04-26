c16e0ba5db9d05b3ba23a7a46c260fd747419fcd
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-04-26 12:22:24 -0600

    conexion a bd
    faltan los datos de la base, y falta sacar los datos del json para comparar en el inicio de sesion

---
+++ conexion.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mysql = require('mysql');
+const port = ;
+const ipAddr = '';
+
+const app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: '',
+    database: '',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Unable to connect to the database.');
+        throw err;
+    } else {
+        console.log('Connected to the database.');
+    }
+});
+
+app.get("/home/:nombre", (req, res) => {
+    db.query("Select nombre, contraseña, adminCuenta From Cuenta Where adminCuenta = 'true'", (err, rows) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            let result = [];
+            for (let row of rows) {
+                result.push({
+                    nombre: row.nombre,
+                    contraseña: row.contraseña,
+                });
+            }
+            res.json(result);
+        }
+    })
+});
